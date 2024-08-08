ecce16f359c898f42264b8e2ad84eb214b75ef83
ADANlicea <adanaliceauk@gmail.com>
2024-08-08 18:59:25 +0200

    creamos archivo

---
+++ 2024.py
@@ -0,0 +1,36 @@
+import math
+"""numero1 = int(input("ingrese el numero 1: "))
+numero2 = int(input("ingrese el numero 2: "))
+numero3 = int(input("ingrese el numero 3: "))
+resultado = numero1 + numero2 + numero3
+print (resultado)
+
+numero1 = input("ingrese el numero 1: ")
+numero2 = input("ingrese el numero 2: ")
+promedio = (numero1+numero2)
+print(promedio)
+
+
+numero1 = int(input("ingrese el numero 1: "))
+numero2 = int(input("ingrese el numero 2: "))
+multiplicacion = math.sqrt(numero1*numero2)
+print(multiplicacion)
+
+
+
+numero1 = int(input("ingrese la cantidad en metros: "))
+conversion1 = numero1*10
+conversion2 = numero1*100
+print("es equivalente a:",conversion1, "decimetros")
+print("es equivalente a:",conversion2, "centrimetros")
+
+numero1 = int(input("ingrese el lado A: "))
+numero2 = int(input("ingrese el lado B: "))
+perimetro = numero1 + numero2
+area = numero1 * numero2
+print("el perimetro es :", perimetro,"y el area es:",area)
+"""
+
+
+
+
