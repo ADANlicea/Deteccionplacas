3f773c8bc8499017bb4e972f2ea7e7b14c294444
ADANlicea <adanaliceauk@gmail.com>
2024-08-14 19:16:06 +0200

    mensaje

---
+++ Clase 3.py
@@ -0,0 +1,20 @@
+#   Días de la semana: Lunes(1), Martes(2), Miercoles (3), Jueves (4), Viernes (5), Sábado (6), Domingo (7)
+# Dia = (lunes, martes, miercoles, jueves, viernes, sabado, domingo)
+numero = int(input("Ingrese el dia de la semana: "))
+if numero == 1:
+    print("lunes")
+elif numero == 2:
+    print("martes")
+elif numero == 3:
+    print("miercoles")
+elif numero == 4:
+    print("jueves")
+elif numero == 5:
+    print("viernes")
+elif numero == 6:
+    print("sabado")
+elif numero == 7:
+    print("domingo")
+else:
+    print("Dia no existente")
+
